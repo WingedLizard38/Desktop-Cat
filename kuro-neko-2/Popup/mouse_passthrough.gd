a8fdc8b271d393d9091e3f12285bd191b4249817
WingedLizard38 <121652611+WingedLizard38@users.noreply.github.com>
2026-08-21 22:50:36 -0400

    Delete kuro-neko-2 directory

--- kuro-neko-2/Popup/mouse_passthrough.gd
+++
@@ -1,8 +0,0 @@
-extends HBoxContainer
-
-@onready var popup = get_parent().get_parent().get_parent()
-
-func _on_check_box_toggled(toggled_on):
-	if Engine.has_singleton("MousePassthrough"):
-		Engine.get_singleton("MousePassthrough").set_passthrough(popup.get_parent().get_window().get_window_id(), true)
-		popup.close_window()
